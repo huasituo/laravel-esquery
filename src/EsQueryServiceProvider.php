41b65842c345c85e3a0799ff91d9ad7fe2f8324a
HuaSiTuo <info@huasituo.com>
2017-06-12 17:55:28 +0800

    Create EsQueryServiceProvider.php

---
+++ src/EsQueryServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Huasituo\Es;
+
+use Illuminate\Support\ServiceProvider;
+
+class EsQueryServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     */
+    public function boot() {
+    }
+
+    /**
+     * Register the service provider.
+     */
+    public function register(){
+        // Temp to use in closure.
+        $app = $this->app;
+
+        $this->app['es'] = $this->app->share(function($app)
+        {
+            return new EsQuery($app);
+        });
+    }
+}
