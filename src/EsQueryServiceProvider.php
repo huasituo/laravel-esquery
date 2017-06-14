f1cd2292215997c838eac1c61f07b53ad7c9f054
Seven <shiweidu@outlook.com>
2017-06-14 11:53:04 +0800

    重构服务提供者～

--- src/EsQueryServiceProvider.php
+++ src/EsQueryServiceProvider.php
@@ -7,29 +7,56 @@ use Illuminate\Support\ServiceProvider;
 class EsQueryServiceProvider extends ServiceProvider
 {
     /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
-
-    /**
      * Bootstrap the application events.
+     *
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
      */
     public function boot()
     {
+        $this->app->singleton('es', function ($app) {
+            return new EsQuery($app);
+        });
     }
 
     /**
-     * Register the service provider.
+     * Register the application.
+     *
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
      */
     public function register()
     {
-        // Temp to use in closure.
-        $app = $this->app;
+        $this->registerAliases();
+    }
 
-        $this->app['es'] = $this->app->share(function ($app) {
-            return new EsQuery($app);
-        });
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function provides()
+    {
+        return ['es'];
+    }
+
+    /**
+     * Register the application aliases.
+     *
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    protected function registerAliases()
+    {
+        $aliases = [
+            'es' => ['\\Huasituo\\Es\\EsQuery'],
+        ];
+
+        foreach ($aliases as $key => $aliases) {
+            foreach ($aliases as $alias) {
+                $this->app->alias($key, $alias);
+            }
+        }
     }
 }
